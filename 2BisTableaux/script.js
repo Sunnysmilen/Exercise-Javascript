3fbf83e161e9e97dc5041be2097470f2b41f6772
nicoletta <davidsalnicole@gmail.com>
2025-03-24 15:17:13 +0100

    created a folder for 2bis exercise

---
+++ 2BisTableaux/script.js
@@ -0,0 +1,68 @@
+const instructors = [
+  {
+    name: "John",
+    availability: "all",
+    specialities: ["Javascript", "Python", "C++"],
+  },
+  {
+    name: "Mary",
+    availability: "weekend",
+    specialities: ["Javascript", "Ruby", "C++"],
+  },
+  {
+    name: "Chris",
+    availability: "evenings",
+    specialities: ["Javascript"],
+  },
+  {
+    name: "Anthony",
+    availability: "all",
+    specialities: ["Python", "Ruby"],
+  },
+  {
+    name: "Pauline",
+    availability: "only Mondays",
+    specialities: ["Javascript", "Html", "CSS"],
+  },
+  {
+    name: "Mark",
+    availability: "all",
+    specialities: ["C#", "C++", "Javascript"],
+  },
+  {
+    name: "Helen",
+    availability: "evenings",
+    specialities: ["Python", "C++"],
+  },
+  {
+    name: "Charles",
+    availability: "none",
+    specialities: ["Python"],
+  },
+];
+
+let instructor;
+console.log(
+  instructors.reduce(weekendInstuctor) 
+
+    if (weekendInstructors = (availability === "weekend") && (availability === "all")) || (specialities === "Javascript")
+  ) weekendInstructors.reduce
+);
+console.log(instructor);
+};
+// const weekendInstructors = instructors.filter(
+//   (instructors) =>
+//     (instructors.specialities.includes("Javascript") &&
+//       instructors.availability === "weekend") ||
+//     instructors.availability === "all"
+// );
+
+// weekendInstructors.forEach((instructor) => {
+//   let message = `Hi ${instructor.name}, we inform you that this weekend you will be doing the support class`;
+
+//   if (instructor.specialities.includes("Python")) {
+//     message += " and you need to prepare a Python workshop";
+//   }
+
+//   console.log(message);
+// });
